e136a445e9b11838e50e2ce9b0b72d80a709a65d
miroslav <miroslav@kolosek.com>
2024-06-27 13:56:13 +0200

    add article test 5

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -53,5 +53,18 @@ RSpec.describe Article, type: :model do
         expect(Article.public_count).to eq 4
       end
     end
+
+    describe "#public_count 5" do
+      let!(:article_1) { Article.create!(title: 'Article 1', body: 'article 1111111', status: 'public') }
+      let!(:article_2) { Article.create!(title: 'Article 2', body: 'article 22222222', status: 'private') }
+      let!(:article_3) { Article.create!(title: 'Article 3', body: 'article 33333333', status: 'public') }
+      let!(:article_4) { Article.create!(title: 'Article 4', body: 'article 44444444', status: 'public') }
+      let!(:article_5) { Article.create!(title: 'Article 5', body: 'article 55555555', status: 'public') }
+      let!(:article_6) { Article.create!(title: 'Article 6', body: 'article 66666666', status: 'public') }
+
+      it do
+        expect(Article.public_count).to eq 5
+      end
+    end
   end
 end
