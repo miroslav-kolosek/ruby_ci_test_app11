3290458a99a2cc294da7bb070299f6944b1d32fd
miroslav <miroslav@kolosek.com>
2024-06-27 13:15:56 +0200

    fix article test 2

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Article, type: :model do
     describe "#public_count 2" do
       let!(:article_1) { Article.create!(title: 'Article 1', body: 'article 1111111', status: 'public') }
       let!(:article_2) { Article.create!(title: 'Article 2', body: 'article 22222222', status: 'private') }
-      let!(:article_1) { Article.create!(title: 'Article 3', body: 'article 33333333', status: 'public') }
+      let!(:article_3) { Article.create!(title: 'Article 3', body: 'article 33333333', status: 'public') }
 
       it do
         expect(Article.public_count).to eq 2
